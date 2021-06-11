ca0a0edece38aaa2f81168cf41d8e9a99d51e4b0
Cnidarian <cmphillips23@gmail.com>
2021-06-11 00:27:42 -0500

    started js and added visibility properties for questions

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
-var startButton = document.querySelector(".start");
+var startButton = document.getElementById("start");
+var title = document.getElementById("title");
+var desc = document.getElementById("desc");
+var questionTitle = document.getElementById("question-title");
+var answers = document.getElementById("choices");
+
+function startQuiz() {
+    startButton.style.visibility = "hidden";
+    title.style.visibility = "hidden";
+    desc.style.visibility = "hidden";
+    questionTitle.style.visibility = "visible";
+    answers.style.visibility = "visible";
+};
